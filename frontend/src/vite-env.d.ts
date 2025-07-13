1d3115937e89616a6b52ce2698b964ead762c937
Salugu Harshita Bhanu <shiki2hustle@gmail.com>
2025-07-13 11:06:34 +0530

    few errors debugged

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,6 +3,11 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
+  readonly VITE_APP_NAME: string;
+  readonly VITE_ENABLE_BLOCKCHAIN: string; // Will be 'true' or 'false'
+  readonly VITE_ANALYTICS_ID: string;
+  readonly VITE_MAX_FILE_SIZE: string; // This is a number, but read as a string
+  readonly VITE_CLOUDINARY_CLOUD_NAME: string;
   // Add other VITE_ variables you use in your project here, for example:
   // readonly VITE_ANOTHER_ENV_VAR: string;
 }
